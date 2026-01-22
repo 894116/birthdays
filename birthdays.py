17c5eee2c7b73c0aab3b1b899d51a48b6b247e32
giuliabordignon <894116@stud.unive.it>
2026-01-22 21:53:22 +0100

    Completed Lab 2

--- birthdays.py
+++ birthdays.py
@@ -23,3 +23,30 @@ def return_birthday(name):
     else:
         print('Sadly, we don\'t have {}\'s birthday.'.format(name))
 
+
+def add_birthday_entry(name, birthday):
+    """Add a new entry to the birthday dictionary"""
+    birthdays[name] = birthday
+
+
+def people_leap_year():
+    """Return the names of the people who are born in a leap years"""
+
+    leap_year_people = []
+    for name, date in birthdays.items():
+        year = int(date.split("/")[-1])
+        if (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0):
+            leap_year_people.append(name)
+
+    return leap_year_people
+
+import calendar
+
+def people_leap_year_calendar():
+    """Return the names of the people who are born in a leap years using the calendar module  """
+    leap_people = []
+    for name, date in birthdays.items():
+        year = int(date[-4:])
+        if calendar.isleap(year):
+            leap_people.append(name)
+    return leap_people
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 #! /usr/bin/env python3
 
-from birthdays import return_birthday
+from birthdays import return_birthday, add_birthday_entry
 
 return_birthday('Albert Einstein')
 return_birthday('Alan Turing')
+
+add_birthday_entry ('Erling Haaland', '07/21/2000')
+add_birthday_entry ('Albert Gore', '03/31/1948')
+add_birthday_entry('Elizabeth Bowes-Lyon', '08/04/1900')
+
+from birthdays import people_leap_year
+print("People born in a leap year:", people_leap_year())
+
+from birthdays import people_leap_year_calendar
+print("People born in a leap year (calendar):", people_leap_year_calendar())
