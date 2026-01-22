845e63aaeeeff2013048cd64c0ae77277ac73e2a
giuliabordignon <894116@stud.unive.it>
2026-01-22 18:10:22 +0100

    Properly documented the code

--- birthdays.py
+++ birthdays.py
@@ -1,3 +1,8 @@
+
+"""
+This module contains a dictionary of some famous people's birthday and utility functions to print available names and retrieve a birthday.
+"""
+
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -6,11 +11,13 @@ birthdays = {
     'Rowan Atkinson': '01/6/1955'}
 
 def print_birthdays():
+    """Print all the available name in the dictionary"""
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
 def return_birthday(name):
+    """Print the birthday of the given person if present in the dictionary"""
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
